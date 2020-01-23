32edb9b2d3b7205ce7147da78b036261cb0aba5a
DucklingTurtle <58099492+DucklingTurtle@users.noreply.github.com>
2020-01-23 14:25:44 -0800

    Update

--- __init__.py
+++ __init__.py
@@ -1,6 +1,43 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
+from forms import RegistrationForm, LoginForm
+app = Flask(__name__)
 
-def create_app():
-    app = Flask(__name__)
+app.config["SECRET_KEY"] = "d2c3113ee16531331a94a16f82b0cd1e"
 
-    return app
+posts = [
+    {
+        'author': 'Corey Schafer',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'April 20, 2018'
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'April 21, 2018'
+    }
+]
+
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template("home.html", posts=posts)
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/register")
+def register():
+    form = RegistrationForm()
+    return render_template("register.html", title="Register", form=form)
+
+@app.route("/login")
+def login():
+    form = LoginForm()
+    return render_template("login.html", title="Login", form=form)
+
+if __name__ == "__main__":
+    app.run(debug=True)
